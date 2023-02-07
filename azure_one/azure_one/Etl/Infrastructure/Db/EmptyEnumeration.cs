22e9adab1b90e6e4c055ab71b28e935bc050e25e
ioedumac <eacevedof@yahoo.es>
2023-02-07 22:42:10 +0100

    etl

--- azure_one/azure_one/Etl/Infrastructure/Db/EmptyEnumeration.cs
+++ azure_one/azure_one/Etl/Infrastructure/Db/EmptyEnumeration.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 namespace azure_one.Etl.Infrastructure.Db
 {
-	public enum EmptyEnumeration
-	{
-	}
+    public struct StructDb1
+    {
+        // string connectionString = "Server=tcp:yourserver.database.windows.net,1433;Initial Catalog=yourdatabase;Persist Security Info=False;User ID=yourusername;Password=yourpassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public const string Server = "Atributo";
+        public const string Port = "1433";
+        public const string Database = "local_laciahub";
+    }
 }
 
