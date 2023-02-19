7679ab4fd82ec0057209766c2308e7e01a877192
ioedumac <eacevedof@yahoo.es>
2023-02-19 12:15:04 +0100

    permisos

--- azure_one/azure_one/Etl/Infrastructure/Db/EmptyEnumeration.cs
+++ azure_one/azure_one/Etl/Infrastructure/Db/EmptyEnumeration.cs
@@ -3,11 +3,20 @@ namespace azure_one.Etl.Infrastructure.Db
 {
     public struct StructDb1
     {
-        // string connectionString = "Server=tcp:yourserver.database.windows.net,1433;Initial Catalog=yourdatabase;Persist Security Info=False;User ID=yourusername;Password=yourpassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        // string connectionString = "Server=tcp:yourserver.database.windows.net,1433;
+        // Initial Catalog=yourdatabase;Persist Security Info=False;User ID=yourusername;
+        // Password=yourpassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-        public const string Server = "Atributo";
+        public const string Server = "";
         public const string Port = "1433";
         public const string Database = "local_laciahub";
+        public const string Username = "sa";
+        public const string Password = "1234";
+
+        public string getConnectionString()
+        {
+            return "";
+        }
     }
 }
 
