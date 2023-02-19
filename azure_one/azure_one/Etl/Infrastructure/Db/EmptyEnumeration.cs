cf8532accac902b77152b0f9ff29657b3bf829d0
ioedumac <eacevedof@yahoo.es>
2023-02-19 13:32:24 +0100

    StructDb1

--- azure_one/azure_one/Etl/Infrastructure/Db/EmptyEnumeration.cs
+++ azure_one/azure_one/Etl/Infrastructure/Db/EmptyEnumeration.cs
@@ -5,17 +5,30 @@ namespace azure_one.Etl.Infrastructure.Db
     {
         // string connectionString = "Server=tcp:yourserver.database.windows.net,1433;
         // Initial Catalog=yourdatabase;Persist Security Info=False;User ID=yourusername;
-        // Password=yourpassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        // Password=yourpassword;MultipleActiveResultSets=False;Encrypt=True;
+        // TrustServerCertificate=False;Connection Timeout=30;";
 
-        public const string Server = "";
-        public const string Port = "1433";
-        public const string Database = "local_laciahub";
-        public const string Username = "sa";
-        public const string Password = "1234";
+        private const string Server = "localhost";
+        private const string Port = "1433";
+        private const string Database = "local_laciahub";
+        private const string Username = "sa";
+        private const string Password = "EafEaf1234";
 
-        public string getConnectionString()
+        public string GetConnectionString()
         {
-            return "";
+            string[] connectionParts = new string[]
+            {
+                $"Server:tcp:{StructDb1.Server},{StructDb1.Port}",
+                $"Initial Catalog={StructDb1.Database}",
+                $"Persist Security Info=False;User ID={StructDb1.Username}",
+                $"Password={StructDb1.Password}",
+                "MultipleActiveResultSets=False",
+                "Encrypt=True",
+                "TrustServerCertificate=False",
+                "Connection Timeout=30"
+            };
+
+            return string.Join(";", connectionParts);
         }
     }
 }
