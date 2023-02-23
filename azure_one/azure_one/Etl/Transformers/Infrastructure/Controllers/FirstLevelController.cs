81a28bde319ef4314e52f436302c06f0cee519c3
ioedumac <eacevedof@yahoo.es>
2023-02-23 20:32:11 +0100

    transformers

--- azure_one/azure_one/Etl/Transformers/Infrastructure/Controllers/FirstLevelController.cs
+++ azure_one/azure_one/Etl/Transformers/Infrastructure/Controllers/FirstLevelController.cs
@@ -1,6 +1,18 @@
+using azure_one.Etl.Transformers.Application;
+
 namespace azure_one.Etl.Transformers.Infrastructure.Controllers;
 
 public sealed class FirstLevelController
 {
+    private static TransformDemoService _transformDemoService;
     
+    public FirstLevelController(TransformDemoService transformDemoService)
+    {
+        _transformDemoService = transformDemoService;
+    }
+
+    public void Invoke()
+    {
+        _transformDemoService.Invoke();
+    }
 }
--- azure_one/azure_one/Startup.cs
+++ azure_one/azure_one/Startup.cs
@@ -5,7 +5,9 @@ using azure_one.Etl.Shared.Infrastructure.Db;
 using azure_one.Etl.Shared.Infrastructure.Repositories;
 using azure_one.Etl.RawLoaders.Application;
 using azure_one.Etl.RawLoaders.Infrastructure;
-
+using azure_one.Etl.Transformers.Application;
+using azure_one.Etl.Transformers.Infrastructure.Controllers;
+using azure_one.Etl.Transformers.Infrastructure.Repositories;
 
 [assembly:FunctionsStartup(typeof(azure_one.Startup))]
 namespace azure_one;
@@ -24,21 +26,14 @@ public class Startup: FunctionsStartup
         //aqui se configura la inyeccion de dependecias
         builder.Services.AddHttpClient();
         
-        //repost
-        builder.Services.AddSingleton<TruncateRepository>(
-            s => new TruncateRepository(new Mssql())
-        );
+        //repositories
+        builder.Services.AddSingleton<TruncateRepository>(s => new TruncateRepository(new Mssql()));
         
         //services
-        builder.Services.AddSingleton<TruncateTableService>(
-            s => new TruncateTableService(new TruncateRepository(new Mssql()))
-        );
-        builder.Services.AddSingleton<LoadLanguagesRawService>(
-            s => new LoadLanguagesRawService()
-        );
-        builder.Services.AddSingleton<LoadCountriesRawServices>(
-            s => new LoadCountriesRawServices()
-        );
+        builder.Services.AddSingleton<TruncateTableService>(s => new TruncateTableService(new TruncateRepository(new Mssql())));
+        builder.Services.AddSingleton<LoadLanguagesRawService>(s => new LoadLanguagesRawService());
+        builder.Services.AddSingleton<LoadCountriesRawServices>(s => new LoadCountriesRawServices());
+        
         
         //controlleres
         builder.Services.AddSingleton<RawLoadersController>(
@@ -47,6 +42,9 @@ public class Startup: FunctionsStartup
                     new LoadLanguagesRawService(), 
                     new LoadCountriesRawServices()
                 )
+        ); 
+        builder.Services.AddSingleton<FirstLevelController>(
+            s => new FirstLevelController(new TransformDemoService(new SqlFilesRepository(new Mssql())))
         );
             
         //fix: No data is available for encoding 1252. For information on defining a custom encoding
