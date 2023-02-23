2c9e52d56723763a51d934613dae476da08a5786
ioedumac <eacevedof@yahoo.es>
2023-02-23 22:38:36 +0100

    ejecuta la query que esta en el archivo

--- azure_one/azure_one/azure_one.cs
+++ azure_one/azure_one/azure_one.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,7 +6,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using azure_one.Etl.RawLoaders.Infrastructure;
-using azure_one.Etl.Shared.Infrastructure.Files;
 using azure_one.Etl.Transformers.Infrastructure.Controllers;
 
 
