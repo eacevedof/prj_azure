5e9936630771ce42e49694e4a5d5979fd719a409
ioedumac <eacevedof@yahoo.es>
2023-02-25 12:55:08 +0100

    hay q corregir countries

--- azure_one/azure_one/Etl/RawLoaders/Application/LoadXlsLanguagesService.cs
+++ azure_one/azure_one/Etl/RawLoaders/Application/LoadXlsLanguagesService.cs
@@ -1,38 +1,33 @@
-using System.Collections.Generic;
+using azure_one.Etl.RawLoaders.Domain.Entities;
 using azure_one.Etl.Shared.Infrastructure.Files;
 using azure_one.Etl.Shared.Infrastructure.Env;
 using azure_one.Etl.Shared.Infrastructure.Log;
 using azure_one.Etl.Shared.Infrastructure.Db;
 using azure_one.Etl.Shared.Infrastructure.Db.QueryBuilders;
-using azure_one.Etl.RawLoaders.Domain.Enums;
 
 namespace azure_one.Etl.RawLoaders.Application;
 
 public sealed class LoadXlsLanguagesService: AbsRawService
 {
-    
     public override void Invoke()
     {
-        string pathExcel = Env.GetConcat("HOME", ExcelToStagingConfigEnum.path_file);
+        ImpLanguagesEntity language = ImpLanguagesEntity.GetInstance();
+        string pathExcel = Env.GetConcat("HOME", language.PathXls);
+        
         ExcelReader excelReader = ExcelReader.FromPrimitives((
             pathExcel, 
-            ExcelToStagingConfigEnum.languages_sheetnr, 
-            ExcelToStagingConfigEnum.languages_max_col
+            language.SheetNr, 
+            language.SheetMaxColumn
         ));
         
         string sql = (
             new BulkInsert(
-                ExcelToStagingConfigEnum.languages_table, 
-                new Dictionary<string, string>() {
-                    { "Column0", "uuid" },
-                    { "Column1", "val" },
-                    { "Column2", "codesap" },
-                    { $"{BulkInsert.TAG_CONSTANT}:hola", "imp_uuid"},
-                    { $"{BulkInsert.TAG_CONSTANT}:finix", "tenant_slug"}
-                },
+                language.Table,
+                language.ColumnMapping,
                 excelReader.GetData()
             )
         ).GetBulkInsertQuery();
+        
         Lg.pr(sql);
         Mssql.GetInstance().Execute(sql);
     }
