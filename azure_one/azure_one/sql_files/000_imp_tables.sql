695b182228d08f5b6e0585535e53db74e5401f94
ioedumac <eacevedof@yahoo.es>
2023-02-25 11:28:01 +0100

    constantes tenant-slug

--- azure_one/azure_one/sql_files/000_imp_tables.sql
+++ azure_one/azure_one/sql_files/000_imp_tables.sql
@@ -1,12 +1,5 @@
 UPDATE [local_staging].[dbo].[imp_languages] 
-SET codesap='xxx'
+SET nok='1'
 WHERE 1=1
 ;
 
-UPDATE [local_staging].[dbo].[imp_languages]
-SET tenant_slug='fini'
-WHERE 1=1
-;
-
-SELECT * 
-FROM [local_staging].[dbo].[imp_languages]
