9fdddc5583c7af839a9069ab8238732ac89e0cb8
ioedumac <eacevedof@yahoo.es>
2023-02-25 13:19:52 +0100

    loadxlscountries

--- azure_one/azure_one/Etl/RawLoaders/Application/LoadXlsCountriesService.cs
+++ azure_one/azure_one/Etl/RawLoaders/Application/LoadXlsCountriesService.cs
@@ -1,10 +1,9 @@
-using System.Collections.Generic;
+using azure_one.Etl.RawLoaders.Domain.Entities;
 using azure_one.Etl.Shared.Infrastructure.Files;
 using azure_one.Etl.Shared.Infrastructure.Env;
 using azure_one.Etl.Shared.Infrastructure.Log;
 using azure_one.Etl.Shared.Infrastructure.Db;
 using azure_one.Etl.Shared.Infrastructure.Db.QueryBuilders;
-using azure_one.Etl.RawLoaders.Domain.Enums;
 
 namespace azure_one.Etl.RawLoaders.Application;
 
@@ -12,30 +11,19 @@ public sealed class LoadXlsCountriesServices: AbsRawService
 {
     public override void Invoke()
     {
-        string pathExcel = Env.GetConcat("HOME", ExcelToStagingConfigEnum.path_file);
+        ImpCountriesEntity countries = ImpCountriesEntity.GetInstance();
+        
+        string pathExcel = Env.GetConcat("HOME", countries.PathXls);
         ExcelReader excelReader = ExcelReader.FromPrimitives((
             pathExcel, 
-            ExcelToStagingConfigEnum.countries_sheetnr, 
-            ExcelToStagingConfigEnum.countries_max_col
+            countries.SheetNr, 
+            countries.SheetMaxColumn
         ));
         
         string sql = (
             new BulkInsert(
-                ExcelToStagingConfigEnum.countries_table, 
-                new Dictionary<string, string>() {
-                    { "Column0", "uuid" },
-                    { "Column1", "val" },
-                    { "Column2", "codesap" },
-                    { "Column3", "tr_1" },
-                    { "Column4", "tr_2" },
-                    { "Column5", "tr_3" },
-                    { "Column6", "tr_4" },
-                    { "Column7", "tr_5" },
-                    { "Column8", "tr_6" },
-                    { "Column9", "tr_7" },
-                    { "Column10", "tr_8" },
-                    { "Column11", "tr_9" },
-                }, 
+                countries.Table, 
+                countries.ColumnMapping, 
                 excelReader.GetData()
             )
         ).GetBulkInsertQuery();
