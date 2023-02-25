7825e15d84d4bc6958ea6203ca05d2423e49f693
ioedumac <eacevedof@yahoo.es>
2023-02-25 13:14:56 +0100

    funciona languages and countries

--- azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpProvincesEntity.cs
+++ azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpProvincesEntity.cs
@@ -1,30 +1,30 @@
-using System.Collections.Generic;
-
 namespace azure_one.Etl.RawLoaders.Domain.Entities;
 
 public sealed class ImpProvinces: AbstractEntity
 {
-    public readonly string PathXls = "/Downloads/data-in.xlsx";
-    
-    public readonly string Table = "imp_provinces";
-    public readonly int SheetNr = 2;
-    public readonly int SheetMaxColumn = 3;
-    
-    public readonly Dictionary<string, string> ColumnMapping = new()
+    private ImpProvinces()
     {
-        { "Column0", "uuid" },
-        { "Column1", "val" },
-        { "Column2", "codesap" },
-        { "Column3", "tr_1" },
-        { "Column4", "tr_2" },
-        { "Column5", "tr_3" },
-        { "Column6", "tr_4" },
-        { "Column7", "tr_5" },
-        { "Column8", "tr_6" },
-        { "Column9", "tr_7" },
-        { "Column10", "tr_8" },
-        { "Column11", "tr_9" },
-    };  
+        PathXls = "/Downloads/data-in.xlsx";
+        Table = "imp_provinces";
+        SheetNr = 1;
+        SheetMaxColumn = 3;
+
+        ColumnMapping = new()
+        {
+            { "Column0", "uuid" },
+            { "Column1", "val" },
+            { "Column2", "codesap" },
+            { "Column3", "tr_1" },
+            { "Column4", "tr_2" },
+            { "Column5", "tr_3" },
+            { "Column6", "tr_4" },
+            { "Column7", "tr_5" },
+            { "Column8", "tr_6" },
+            { "Column9", "tr_7" },
+            { "Column10", "tr_8" },
+            { "Column11", "tr_9" },
+        };     
+    }
     
     public static ImpProvinces GetInstance()
     {
