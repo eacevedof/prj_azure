531d10987c721d385046be39d1d08e2b9c303a06
ioedumac <eacevedof@yahoo.es>
2023-02-25 16:43:10 +0100

    guardo errores

--- azure_one/azure_one/azure_one.cs
+++ azure_one/azure_one/azure_one.cs
@@ -2,13 +2,13 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
 using azure_one.Etl.RawLoaders.Infrastructure;
 using azure_one.Etl.Shared.Infrastructure.Repositories;
 using azure_one.Etl.SqlRunners.Infrastructure.Controllers;
-
 
 //test c# online
 //https://techiedelight.com/compiler/
