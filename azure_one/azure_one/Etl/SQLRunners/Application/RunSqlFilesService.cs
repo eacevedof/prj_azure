a76584348cb576f8511fded6a115904f598af368
ioedumac <eacevedof@yahoo.es>
2023-02-25 11:43:43 +0100

    RunSqlRiles

--- azure_one/azure_one/Etl/SQLRunners/Application/RunSqlFilesService.cs
+++ azure_one/azure_one/Etl/SQLRunners/Application/RunSqlFilesService.cs
@@ -2,10 +2,10 @@ using azure_one.Etl.SQLRunners.Infrastructure.Repositories;
 
 namespace azure_one.Etl.SQLRunners.Application;
 
-public sealed class TransformDemoService
+public sealed class RunSqlFilesService
 {
     private readonly SqlFilesRepository _sqlFilesRepository;
-    public TransformDemoService(SqlFilesRepository sqlFilesRepository)
+    public RunSqlFilesService(SqlFilesRepository sqlFilesRepository)
     {
         _sqlFilesRepository = sqlFilesRepository;
     }
