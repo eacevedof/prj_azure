69d942f8cd2cb11f4f2a6305bde2f6c9376fae46
ioedumac <eacevedof@yahoo.es>
2023-02-25 12:55:08 +0100

    hay q corregir countries

---
+++ azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpCountriesEntity.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace azure_one.Etl.RawLoaders.Domain.Entities;
+
+public sealed class ImpCountriesEntity : EntityInterface
+{
+    public readonly string PathXls = "/Downloads/data-in.xlsx";
+    public readonly string Table = "imp_countries";
+    public readonly int SheetNr = 2;
+    public readonly int SheetMaxColumn = 3;
+
+    public readonly Dictionary<string, string> ColumnMapping = new()
+    {
+        { "Column0", "uuid" },
+        { "Column1", "val" },
+        { "Column2", "codesap" },
+        { "Column3", "tr_1" },
+        { "Column4", "tr_2" },
+        { "Column5", "tr_3" },
+        { "Column6", "tr_4" },
+        { "Column7", "tr_5" },
+        { "Column8", "tr_6" },
+        { "Column9", "tr_7" },
+        { "Column10", "tr_8" },
+        { "Column11", "tr_9" },
+    };
+
+    public static ImpCountriesEntity GetInstance()
+    {
+        return new ImpCountriesEntity();
+    }
+}
---
+++ azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpLanguagesEntity.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using azure_one.Etl.Shared.Infrastructure.Db.QueryBuilders;
+
+namespace azure_one.Etl.RawLoaders.Domain.Entities;
+
+public sealed class ImpLanguagesEntity: EntityInterface
+{
+    public readonly string PathXls = "/Downloads/data-in.xlsx";
+    
+    public readonly string Table = "imp_languages";
+    public readonly int SheetNr = 0;
+    public readonly int SheetMaxColumn = 2;
+    
+    public readonly Dictionary<string, string> ColumnMapping = new()
+    {
+        { "Column0", "uuid" },
+        { "Column1", "val" },
+        { "Column2", "codesap" },
+        { $"{BulkInsert.TAG_CONSTANT}:hola", "imp_uuid"},
+        { $"{BulkInsert.TAG_CONSTANT}:finix", "tenant_slug"},
+    };
+    
+    public static ImpLanguagesEntity GetInstance()
+    {
+        return new ImpLanguagesEntity();
+    }    
+}
