ff1172ae5caf1a06bbe258b74c2d1a53e37be00f
ioedumac <eacevedof@yahoo.es>
2023-02-25 19:01:31 +0100

    backup

---
+++ azure_one/azure_one/Etl/Shared/Infrastructure/Db/Paginator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace azure_one.Etl.Shared.Infrastructure.Db;
+
+public sealed class Paginator
+{
+    public static List<Tuple<int, int>> GetPages(int numItems, int pageSize)
+    {
+        if (numItems <= 0) return new List<Tuple<int, int>>{new(0,0)};
+        if (pageSize <= 0)
+            return new List<Tuple<int, int>>
+            {
+                new(0, numItems - 1)
+            };
+        int numPages = numItems / pageSize;
+        int inLastPage = numItems % pageSize;
+        int withResidual = inLastPage > 0 ? numPages + 1 : numPages;
+
+        List<Tuple<int, int>> result = new ();
+        int pageSizeMinus1 = pageSize - 1;
+        for(int page=0; page<=(withResidual-1); page++)
+            result.Add(new Tuple<int, int>(page * pageSize, page + pageSizeMinus1));
+        
+        return result;
+    }
+}
--- azure_one/azure_one/Etl/Shared/Infrastructure/Db/QueryBuilders/BulkInsert.cs
+++ azure_one/azure_one/Etl/Shared/Infrastructure/Db/QueryBuilders/BulkInsert.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
+using azure_one.Etl.Shared.Infrastructure.Db;
 
 namespace azure_one.Etl.Shared.Infrastructure.Db.QueryBuilders;
+
 
 public sealed class BulkInsert
 {
     private readonly string _targetTable;
     private readonly Dictionary<string, string> _columnMapping;
     private readonly List<Dictionary<string, string>> _dataRows;
+    
     public const string TAG_CONSTANT = "constant"; 
 
     public BulkInsert(
@@ -18,7 +21,7 @@ public sealed class BulkInsert
         _columnMapping = columnMapping;
         _dataRows = dataRows;
     }
-
+    
     public string GetBulkInsertQuery()
     {
         List<string> insValues = new List<string>();
@@ -28,11 +31,47 @@ public sealed class BulkInsert
             insValues.Add(strvalues);
         }
 
-        string sql = GetInsertIntoHeader();
-        sql += string.Join(",", insValues);
-        sql += ";";
+        List<List<string>> splitted = Get1000Splitted(insValues);
+
+        string sql = "";
+        foreach (List<string> batchInsVals in splitted)
+        {
+            sql += GetInsertIntoHeader();
+            sql += string.Join(",", batchInsVals);
+            sql += ";";
+        }
         return sql;
     }
+    
+    public List<List<string>> Get1000Splitted(List<string> insValues)
+    {
+        List<List<string>> in1000 = new();
+        int count = 0;
+        List<string> tmp = new();
+        foreach (string value in insValues)
+        {
+            tmp.Add(value);
+            if ((count % 1000) == 0)
+            {
+                in1000.Add(tmp);
+                count = 0;
+                tmp = new();
+            }
+            count++;
+        }
+        return in1000;
+    }
+    
+    private string GetInsertIntoHeader()
+    {
+        List<string> values = new List<string>()
+        {
+            $"INSERT INTO [local_staging].[dbo].[{_targetTable}] (",
+            string.Join(",",_columnMapping.Values),
+            ") VALUES"
+        };
+        return string.Join("", values);
+    }        
     
     private string GetValuesBetweenParenthesis(Dictionary<string, string> row)
     {
@@ -59,14 +98,4 @@ public sealed class BulkInsert
         return parts[1] ?? "";
     }
 
-    private string GetInsertIntoHeader()
-    {
-        List<string> values = new List<string>()
-        {
-            $"INSERT INTO [local_staging].[dbo].[{_targetTable}] (",
-            string.Join(",",_columnMapping.Values),
-            ") VALUES"
-        };
-        return string.Join("", values);
-    }    
 }
