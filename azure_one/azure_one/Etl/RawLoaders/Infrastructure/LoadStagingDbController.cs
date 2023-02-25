923411bff0d6a8175ac5644432e6cebefbb9dac8
ioedumac <eacevedof@yahoo.es>
2023-02-25 14:02:05 +0100

    provinces

--- azure_one/azure_one/Etl/RawLoaders/Infrastructure/LoadStagingDbController.cs
+++ azure_one/azure_one/Etl/RawLoaders/Infrastructure/LoadStagingDbController.cs
@@ -7,16 +7,19 @@ public sealed class LoadStagingDbController
     private readonly TruncateImpTablesService _truncateImpTablesService;
     private readonly LoadXlsLanguagesService _loadXlsLanguagesService;
     private readonly LoadXlsCountriesServices _loadXlsCountriesServices;
+    private readonly LoadXlsProvincesService _loadXlsProvincesService;
     
     public LoadStagingDbController(
         TruncateImpTablesService truncateImpTablesService,
         LoadXlsLanguagesService loadXlsLanguagesService,
-        LoadXlsCountriesServices loadXlsCountriesServices
+        LoadXlsCountriesServices loadXlsCountriesServices,
+        LoadXlsProvincesService loadXlsProvincesService
     )
     {
         _loadXlsLanguagesService = loadXlsLanguagesService;
         _loadXlsCountriesServices = loadXlsCountriesServices;
         _truncateImpTablesService = truncateImpTablesService;
+        _loadXlsProvincesService = loadXlsProvincesService;
     }
 
     public void Invoke()
@@ -24,5 +27,6 @@ public sealed class LoadStagingDbController
         _truncateImpTablesService.Invoke();
         _loadXlsLanguagesService.Invoke();
         _loadXlsCountriesServices.Invoke();
+        _loadXlsProvincesService.Invoke();
     }
 }
