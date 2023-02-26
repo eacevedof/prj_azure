773d95d871f16eb529b20b9136154e0f6d3b5510
ioedumac <eacevedof@yahoo.es>
2023-02-26 16:43:59 +0100

    imp-companies

---
+++ azure_one/azure_one/sql_files/post_load/0090_imp_companies.sql
@@ -0,0 +1,74 @@
+
+
+UPDATE [local_staging].[dbo].[imp_companies] SET companies_id=NULL;
+
+UPDATE imp
+SET imp.companies_id = mt.id
+FROM [local_laciahub].[dbo].[companies] mt
+INNER JOIN [local_staging].[dbo].[imp_companies] imp
+-- ON mt.name = imp.val
+ON mt.id = imp.uuid
+WHERE 1=1
+AND imp.nok IS NULL
+;
+
+UPDATE imp
+SET imp.city_id = mt.id
+FROM [local_laciahub].[dbo].[cities] mt
+INNER JOIN [local_staging].[dbo].[imp_companies] imp
+ON mt.id = imp.city_uuid
+WHERE 1=1
+AND imp.nok IS NULL
+;
+
+UPDATE imp
+SET imp.country_id = mt.countries_id
+FROM [local_laciahub].[dbo].[cities] mt
+INNER JOIN [local_staging].[dbo].[imp_companies] imp
+ON mt.id = imp.city_uuid
+WHERE 1=1
+AND imp.nok IS NULL
+;
+
+UPDATE [local_staging].[dbo].[imp_companies] SET nok = 1 WHERE city_id IS NULL;
+UPDATE [local_staging].[dbo].[imp_companies] SET nok = 1 WHERE countries_id IS NULL;
+
+UPDATE mt
+SET
+mt.name = CONVERT(VARCHAR(50), imp.val),
+mt.updated_at = GETDATE()
+FROM [local_laciahub].[dbo].[companies] mt
+INNER JOIN [local_staging].[dbo].[imp_companies] imp
+ON mt.id = imp.companies_id
+WHERE 1=1
+AND imp.nok IS NULL
+;
+
+INSERT INTO [local_laciahub].[dbo].[companies]
+(
+city_id,
+name, 
+created_at
+)
+SELECT
+    imp.city_id,
+    CONVERT(VARCHAR(50),imp.val) name,
+    GETDATE()
+FROM [local_staging].[dbo].[imp_companies] imp
+LEFT JOIN [local_laciahub].[dbo].[companies] mt
+ON mt.id = imp.companies_id
+WHERE 1=1
+AND imp.nok IS NULL
+AND mt.id IS NULL
+;
+
+-- actualizo los ids de los nuevos insertados
+UPDATE imp
+SET imp.companies_id = mt.id
+FROM [local_laciahub].[dbo].[companies] mt
+INNER JOIN [local_staging].[dbo].[imp_companies] imp
+ON mt.name = imp.val
+WHERE 1=1
+AND imp.nok IS NULL
+AND imp.companies_id IS NULL;
+;
