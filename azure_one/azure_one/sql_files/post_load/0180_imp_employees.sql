614a1f115676643ab43924bf67fb483fe1419ea3
ioedumac <eacevedof@yahoo.es>
2023-02-28 23:27:04 +0100

    employees fix

--- azure_one/azure_one/sql_files/post_load/0180_imp_employees.sql
+++ azure_one/azure_one/sql_files/post_load/0180_imp_employees.sql
@@ -15,26 +15,28 @@ AND imp.nok IS NULL
 -- crear empleados
 INSERT INTO [local_laciahub].[dbo].[employees]
 (
-    users_id, companies_id, employee_token, employee_code, employee_photo, name, email, password, password_expire_in, remember_token,
-    user_active, user_blocked, max_attempts, language, multisession_allowed, 
+    users_id, companies_id, employee_token, employee_code, employee_photo, employee_name, 
+    employee_surname_1, employee_surname_2, 
+    -- employee_phone, 
+    employee_email, employee_status, email_notification,
     created_at
 )
 SELECT
-    users_id, company_id, 
+    imp.users_id, imp.company_id companies_id, 
     (SELECT UPPER(CONVERT(VARCHAR(25), REPLACE(NEWID(), '-','')))) employee_token,
-    CONVERT(VARCHAR(9),(SELECT CONVERT(VARCHAR,id)+CONVERT(VARCHAR(45),CONVERT(INT,SELECT()*1000000000)))) employee_code,
+    CONVERT(VARCHAR(9),(SELECT CONVERT(VARCHAR,imp.id)+CONVERT(VARCHAR(45),CONVERT(INT,RAND()*1000000000)))) employee_code,
     NULL employee_photo,
-    CONVERT(VARCHAR(45), employee_name) employee_name, 
-    CONVERT(VARCHAR(45), employee_surname_1) employee_surname_1, 
-    CONVERT(VARCHAR(45), employee_surname_2) employee_surname_2,
+    CONVERT(VARCHAR(45), imp.employee_name) employee_name, 
+    CONVERT(VARCHAR(45), imp.employee_surname_1) employee_surname_1, 
+    CONVERT(VARCHAR(45), imp.employee_surname_2) employee_surname_2,
     -- CONVERT(VARCHAR(45), employee_phone) employee_phone,
-    CONVERT(VARCHAR(45), employee_email) employee_email,
+    CONVERT(VARCHAR(45), imp.employee_email) employee_email,
     1 employee_status,
-    1 email_notification
+    1 email_notification,
     GETDATE() created_at
 FROM [local_staging].[dbo].[imp_employees] imp
 LEFT JOIN [local_laciahub].[dbo].[employees] mt
-ON mt.email = imp.employee_email
+ON mt.employee_email = imp.employee_email
 WHERE 1=1
 AND imp.nok IS NULL
 AND mt.id IS NULL
