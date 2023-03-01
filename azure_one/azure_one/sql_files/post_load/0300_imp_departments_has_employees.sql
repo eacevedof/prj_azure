cf63e40dadee0e5fe5c40e4e9e32af7d7ff1ee45
ioedumac <eacevedof@yahoo.es>
2023-03-01 22:45:58 +0100

    depts-emps

---
+++ azure_one/azure_one/sql_files/post_load/0300_imp_departments_has_employees.sql
@@ -0,0 +1,48 @@
+DELETE 
+FROM [local_laciahub].[dbo].[departments_has_employees]
+WHERE 1=1
+AND NOT EXISTS (
+    SELECT imp.employees_id, imp.employees_departments_id
+    FROM [local_staging].[dbo].[imp_employees] imp
+    WHERE 1=1
+    AND [local_laciahub].[dbo].[departments_has_employees].departments_id = imp.employees_departments_id
+    AND [local_laciahub].[dbo].[departments_has_employees].employees_id = imp.employees_id
+)
+;
+
+/*
+UPDATE mt
+  SET 
+    mt.departments_id = imp.employees_departments_id,
+    mt.updated_at = GETDATE()
+
+-- SELECT mt.departments_id, mt.employees_id, imp.employees_id, imp.employees_departments_id, mt.updated_at
+FROM [local_laciahub].[dbo].[departments_has_employees]  mt
+INNER JOIN [local_staging].[dbo].[imp_employees] imp
+ON mt.employees_id = imp.employees_id
+AND mt.departments_id != imp.employees_departments_id
+WHERE 1=1
+AND imp.nok IS NULL
+-- ORDER BY mt.employees_id, mt.departments_id
+;
+*/
+
+
+INSERT INTO [local_laciahub].[dbo].[departments_has_employees]
+(departments_id, employees_id, created_at)
+SELECT 
+    DISTINCT 
+    imp.employees_departments_id, 
+    imp.employees_id, 
+    GETDATE() created_at
+FROM [local_staging].[dbo].[imp_employees] imp
+WHERE 1=1
+AND imp.nok IS NULL
+AND NOT EXISTS (
+  SELECT departments_id, employees_id
+  FROM [local_laciahub].[dbo].[departments_has_employees] mt
+  WHERE 1=1
+  AND mt.departments_id = imp.employees_departments_id
+  AND mt.employees_id = imp.employees_id
+)
+;
