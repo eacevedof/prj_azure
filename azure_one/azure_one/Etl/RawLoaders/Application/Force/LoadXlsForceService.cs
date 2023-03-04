6639430d238bd0aa0deb0c2efe6582aed840c018
ioedumac <eacevedof@yahoo.es>
2023-03-04 19:05:49 +0100

    json load xls force

--- azure_one/azure_one/Etl/RawLoaders/Application/Force/LoadXlsForceService.cs
+++ azure_one/azure_one/Etl/RawLoaders/Application/Force/LoadXlsForceService.cs
@@ -11,7 +11,7 @@ public sealed class LoadXlsForceService: AbsRawService
 {
     public override void Invoke()
     {
-        ExcelMapper excelMapper = ExcelMapper.GetInstance("000400_cities");
+        ExcelMapper excelMapper = ExcelMapper.GetInstance("000100_force");
         ExcelReader excelReader = ExcelReader.FromPrimitivesSheetName((
             excelMapper.Source["path"],
             excelMapper.Source["sheet_name"], 
