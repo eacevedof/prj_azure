32085438b6f12f63bf5a72b8a4d0ba7685aed597
ioedumac <eacevedof@yahoo.es>
2023-03-04 18:35:43 +0100

    making force loader

---
+++ azure_one/azure_one/Etl/RawLoaders/Application/Force/LoadXlsForceService.cs
@@ -0,0 +1,32 @@
+using System;
+using azure_one.Etl.RawLoaders.Domain.Entities;
+using azure_one.Etl.Shared.Infrastructure.Files;
+using azure_one.Etl.Shared.Infrastructure.Log;
+using azure_one.Etl.Shared.Infrastructure.Db;
+using azure_one.Etl.Shared.Infrastructure.Db.QueryBuilders;
+
+namespace azure_one.Etl.RawLoaders.Application.Force;
+
+public sealed class LoadXlsForceService: AbsRawService
+{
+    public override void Invoke()
+    {
+        ExcelMapper excelMapper = ExcelMapper.GetInstance("000400_cities");
+        ExcelReader excelReader = ExcelReader.FromPrimitivesSheetName((
+            excelMapper.Source["path"],
+            excelMapper.Source["sheet_name"], 
+            Int32.Parse(excelMapper.Source["sheet_max_col"])
+        ));
+        
+        string sql = (
+            new BulkInsert(
+                excelMapper.Target["table"],
+                excelMapper.Mapping,
+                excelReader.GetData(excelMapper.Mapping)
+            )
+        ).GetBulkInsertQuery();
+        
+        Lg.pr(sql);
+        Mssql.GetInstance().Execute(sql);
+    }
+}
