1ac1395a2e9fa64b467ebc707ac621e325b0232b
ioedumac <eacevedof@yahoo.es>
2023-03-04 17:19:28 +0100

    jsons

--- azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpEmployeesDepartmentsEntity.cs
+++ azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpEmployeesDepartmentsEntity.cs
@@ -4,7 +4,7 @@ public sealed class ImpEmployeesDepartmentsEntity : AbstractEntity
 {
     private ImpEmployeesDepartmentsEntity()
     {
-        Table = "imp_employees_departments";
+        Table = "";
      
         PathXls = "/Downloads/data-in.xlsx";
         SheetNr = 9;
@@ -12,18 +12,7 @@ public sealed class ImpEmployeesDepartmentsEntity : AbstractEntity
 
         ColumnMapping = new()
         {
-            { "Column0", "uuid" },
-            { "Column1", "val" },
-            { "Column2", "codesap" },
-            { "Column3", "tr_1" },
-            { "Column4", "tr_2" },
-            { "Column5", "tr_3" },
-            { "Column6", "tr_4" },
-            { "Column7", "tr_5" },
-            { "Column8", "tr_6" },
-            { "Column9", "tr_7" },
-            { "Column10", "tr_8" },
-            { "Column11", "tr_9" },
+     
         };     
     }
     
