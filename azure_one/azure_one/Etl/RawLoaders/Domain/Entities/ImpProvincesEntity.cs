bd35278bcf8f02b5074c4559ca6fc79cf0bdc27d
ioedumac <eacevedof@yahoo.es>
2023-03-04 17:43:19 +0100

    jsons

--- azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpProvincesEntity.cs
+++ azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpProvincesEntity.cs
@@ -12,20 +12,7 @@ public sealed class ImpProvincesEntity: AbstractEntity
 
         ColumnMapping = new()
         {
-            { "Column0", "countries_uuid" },
-            { "Column1", "uuid" },
-            { "Column2", "val" },
-            { "Column3", "codesap" },
-            
-            { "Column4", "tr_1" },
-            { "Column5", "tr_2" },
-            { "Column6", "tr_3" },
-            { "Column7", "tr_4" },
-            { "Column8", "tr_5" },
-            { "Column9", "tr_6" },
-            { "Column10", "tr_7" },
-            { "Column11", "tr_8" },
-            { "Column12", "tr_9" },
+  
         };     
     }
     
