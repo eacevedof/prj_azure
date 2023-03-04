95b63298d65437717ec428284d0e79ae7ef7216d
ioedumac <eacevedof@yahoo.es>
2023-03-04 18:35:43 +0100

    making force loader

---
+++ azure_one/azure_one/Etl/RawLoaders/Infrastructure/LoadForceStagingDbController.cs
@@ -0,0 +1,64 @@
+using azure_one.Etl.RawLoaders.Application;
+
+namespace azure_one.Etl.RawLoaders.Infrastructure;
+
+public sealed class LoadForceStagingDbController
+{
+    private readonly LoadXlsLanguagesService _loadXlsLanguagesService;
+    private readonly LoadXlsCountriesServices _loadXlsCountriesServices;
+    private readonly LoadXlsProvincesService _loadXlsProvincesService;
+    private readonly LoadXlsCitiesService _loadXlsCitiesService;
+    private readonly LoadXlsCompaniesService _loadXlsCompaniesService;
+    private readonly LoadXlsLanguagesCompanyCustomService _loadXlsLanguagesCompanyCustomService;
+    private readonly LoadXlsUserTypesService _loadXlsUserTypesService;
+    private readonly LoadXlsStatusEmployeesService _loadXlsStatusEmployeesService;
+    private readonly LoadXlsEmployeesPositionsService _loadXlsEmployeesPositionsService;
+    private readonly LoadXlsEmployeesDepartmentsService _loadXlsEmployeesDepartmentsService;
+    private readonly LoadXlsRolesService _loadXlsRolesService;
+    private readonly LoadXlsEmployeesService _loadXlsEmployeesService;
+    
+    public LoadForceStagingDbController(
+        LoadXlsLanguagesService loadXlsLanguagesService,
+        LoadXlsCountriesServices loadXlsCountriesServices,
+        LoadXlsProvincesService loadXlsProvincesService,
+        LoadXlsCitiesService loadXlsCitiesService,
+        LoadXlsCompaniesService loadXlsCompaniesService,
+        LoadXlsLanguagesCompanyCustomService loadXlsLanguagesCompanyCustomService,
+        LoadXlsUserTypesService loadXlsUserTypesService,
+        LoadXlsStatusEmployeesService loadXlsStatusEmployeesService,
+        LoadXlsEmployeesPositionsService loadXlsEmployeesPositionsService,
+        LoadXlsEmployeesDepartmentsService loadXlsEmployeesDepartmentsService,
+        LoadXlsRolesService loadXlsRolesService,
+        LoadXlsEmployeesService loadXlsEmployeesService
+    )
+    {
+        _loadXlsLanguagesService = loadXlsLanguagesService;
+        _loadXlsCountriesServices = loadXlsCountriesServices;
+        _loadXlsProvincesService = loadXlsProvincesService;
+        _loadXlsCitiesService = loadXlsCitiesService;
+        _loadXlsCompaniesService = loadXlsCompaniesService;
+        _loadXlsLanguagesCompanyCustomService = loadXlsLanguagesCompanyCustomService;
+        _loadXlsUserTypesService = loadXlsUserTypesService;
+        _loadXlsStatusEmployeesService = loadXlsStatusEmployeesService;
+        _loadXlsEmployeesPositionsService = loadXlsEmployeesPositionsService;
+        _loadXlsEmployeesDepartmentsService = loadXlsEmployeesDepartmentsService;
+        _loadXlsRolesService = loadXlsRolesService;
+        _loadXlsEmployeesService = loadXlsEmployeesService;
+    }
+
+    public void Invoke()
+    {
+        _loadXlsLanguagesService.Invoke();
+        _loadXlsCountriesServices.Invoke();
+        _loadXlsProvincesService.Invoke();
+        _loadXlsCitiesService.Invoke();
+        _loadXlsCompaniesService.Invoke();
+        _loadXlsLanguagesCompanyCustomService.Invoke();
+        _loadXlsUserTypesService.Invoke();
+        _loadXlsStatusEmployeesService.Invoke();
+        _loadXlsEmployeesPositionsService.Invoke();
+        _loadXlsEmployeesDepartmentsService.Invoke();
+        _loadXlsRolesService.Invoke();
+        _loadXlsEmployeesService.Invoke();
+    }
+}
