024f5ed5ea4a9f5fc642b62eb5317ba570ac71c4
ioedumac <eacevedof@yahoo.es>
2023-03-04 17:32:33 +0100

    jsons

--- azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpLanguagesCompanyCustomEntity.cs
+++ azure_one/azure_one/Etl/RawLoaders/Domain/Entities/ImpLanguagesCompanyCustomEntity.cs
@@ -6,7 +6,7 @@ public sealed class ImpLanguagesCompanyCustomEntity: AbstractEntity
 {
     private ImpLanguagesCompanyCustomEntity()
     {
-        Table = "imp_languages_company_custom";
+        Table = "";
         
         PathXls = "/Downloads/data-in.xlsx";
         SheetNr = 6;
@@ -14,11 +14,7 @@ public sealed class ImpLanguagesCompanyCustomEntity: AbstractEntity
 
         ColumnMapping = new()
         {
-            { "Column0", "companies_uuid" },
-            { "Column1", "lang_from" },
-            { "Column2", "lang_tr" },
-            { "Column3", "value_tr" },
-            { "Column4", "tr_num" },
+   
             //{ $"{BulkInsert.TAG_CONSTANT}:hola", "imp_uuid"},
             //{ $"{BulkInsert.TAG_CONSTANT}:finix", "tenant_slug"},
         };        
