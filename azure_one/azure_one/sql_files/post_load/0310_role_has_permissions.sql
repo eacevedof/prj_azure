8a919d1c14c2260ab3b1e74bc629084f632c45f7
ioedumac <eacevedof@yahoo.es>
2023-03-06 23:14:35 +0100

    imp-permissions

--- azure_one/azure_one/sql_files/post_load/0310_role_has_permissions.sql
+++ azure_one/azure_one/sql_files/post_load/0310_role_has_permissions.sql
@@ -1,20 +1,17 @@
-
-UPDATE imp
-SET imp.permissions_id = mt.id
-FROM [local_laciahub].[dbo].[permissions]  mt
-INNER JOIN [local_staging].[dbo].[imp_permissions] imp
-ON mt.slug = imp.slug
+/*
+--id, role_id, permission_id
+select * from role_has_permissions
+*/
+INSERT INTO [local_laciahub].[dbo].[role_has_permissions] (role_id, permission_id)
+SELECT imp.roles_id, imp.permission_id
+FROM [local_staging].[dbo].[imp_permissions] imp
 WHERE 1=1
 AND imp.nok IS NULL
-;
-
-
-UPDATE imp
-SET imp.roles_id = mt.id
-FROM [local_laciahub].[dbo].[roles]  mt
-INNER JOIN [local_staging].[dbo].[imp_permissions] imp
-ON mt.id = imp.roles_uuid
-WHERE 1=1
-AND imp.nok IS NULL
-;
-
+AND imp.permission_type = 'by-role'
+AND NOT EXISTS (
+  SELECT mt.role_id, mt.permission_id
+  FROM [local_laciahub].[dbo].[role_has_permissions] mt
+  WHERE 1=1
+  AND mt.role_id = imp.roles_id
+  AND mt.permission_id = imp.permissions_id
+)
