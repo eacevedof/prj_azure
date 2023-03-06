a541cad538f32389c7cd724a2a128cee5afe8038
ioedumac <eacevedof@yahoo.es>
2023-03-06 23:14:35 +0100

    imp-permissions

--- azure_one/azure_one/sql_files/post_load/0320_model_has_permissions.sql
+++ azure_one/azure_one/sql_files/post_load/0320_model_has_permissions.sql
@@ -1,4 +1,8 @@
 /*
+-- permisos de assets
+-- permission_id (), model_type, model_id (por Modules\Asset\Models\Type)
+select * from model_has_permissions
+
 -- permisos por assets
 access_to_assets
 assets_creation
@@ -13,3 +17,17 @@ assets_relationship_edition
 assets_updating
 assets_versions_review
 */
+INSERT INTO [local_laciahub].[dbo].[model_has_permissions] (model_id, permission_id, model_type)
+SELECT imp.entity_id, imp.permission_id, 'Modules\Asset\Models\Type' model_type
+FROM [local_staging].[dbo].[imp_permissions] imp
+WHERE 1=1
+AND imp.nok IS NULL
+AND imp.permission_type = 'by-asset-type'
+AND NOT EXISTS (
+  SELECT mt.model_id, mt.permission_id
+  FROM [local_laciahub].[dbo].[model_has_permissions] mt
+  WHERE 1=1
+  AND mt.model_type = 'Modules\Asset\Models\Type'
+  AND mt.model_id = imp.entity_id
+  AND mt.permission_id = imp.permissions_id
+)
