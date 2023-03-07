c4cc365852c6e4abacc77cac08e6c1928033c74c
ioedumac <eacevedof@yahoo.es>
2023-03-07 23:07:53 +0100

    asset type

---
+++ azure_one/azure_one/sql_files/post_load/0235_imp_assets_types_tr.sql
@@ -0,0 +1,44 @@
+UPDATE mt
+SET
+    mt.name = CONVERT(VARCHAR(255), imp.tr),
+    mt.updated_at = GETDATE()
+FROM [local_laciahub].[dbo].[assets_types_tr]  mt
+INNER JOIN (
+    SELECT DISTINCT
+    vc_tr.mt_id as assets_types_id, 
+    vli.lang_from as locale,
+    CONVERT(VARCHAR(255),vc_tr.tr_i) as tr
+    FROM view_assets_types_tr [vc_tr]
+    INNER JOIN view_languages_index [vli]
+    ON [vc_tr].[tr_num] = [vli].[tr_num]
+) imp
+ON mt.assets_types_id = imp.assets_types_id
+AND mt.locale = imp.locale
+WHERE 1=1
+;
+
+
+-- antes de ejecutar las trads debo cargar todas las maestras
+INSERT INTO [local_laciahub].[dbo].[assets_types_tr]
+(assets_types_id, locale, name, created_at)
+
+SELECT * 
+FROM (
+    SELECT DISTINCT
+        vc_tr.mt_id as assets_types_id, 
+        vli.lang_from as locale, 
+        CONVERT(VARCHAR(255),vc_tr.tr_i) as tr, 
+        GETDATE() created_at 
+    FROM view_assets_types_tr [vc_tr]
+    INNER JOIN view_languages_index [vli]
+    ON [vc_tr].[tr_num] = [vli].[tr_num]
+) AS imp
+WHERE 1=1
+AND NOT EXISTS 
+(
+    SELECT id
+    FROM [local_laciahub].[dbo].[assets_types_tr] mt
+    WHERE 1=1
+    AND mt.assets_types_id = imp.assets_types_id
+    AND mt.locale = imp.locale
+)
