899063c570eb1da5727d02194a12c60975e6f6d3
ioedumac <eacevedof@yahoo.es>
2023-03-11 23:48:35 +0100

    contexts

---
+++ azure_one/azure_one/Etl/Shared/Infrastructure/Db/Contexts/Context.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using azure_one.Etl.Shared.Infrastructure.Db;
+
+namespace azure_one.Etl.Shared.Infrastructure.Db.Contexts;
+
+
+public sealed class Context
+{
+  
+}
