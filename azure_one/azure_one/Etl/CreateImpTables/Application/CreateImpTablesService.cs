6366359e01d533245de55bc7880326b8aa43c47d
ioedumac <eacevedof@yahoo.es>
2023-03-12 19:03:13 +0100

    create tables ok. sin vistas

--- azure_one/azure_one/Etl/CreateImpTables/Application/CreateImpTablesService.cs
+++ azure_one/azure_one/Etl/CreateImpTables/Application/CreateImpTablesService.cs
@@ -1,3 +1,5 @@
+using azure_one.Etl.CreateImpTables.Infrastructure.Repositories;
+using azure_one.Etl.Shared.Infrastructure.Db;
 using azure_one.Etl.Shared.Infrastructure.Global;
 using azure_one.Etl.Shared.Infrastructure.Db.Contexts;
 
@@ -5,8 +7,16 @@ namespace azure_one.Etl.InstallImpTables.Application;
 
 public sealed class CreateImpTablesService
 {
+    private readonly CreateImpTablesRepository _createImpTablesRepository;
+
+    public CreateImpTablesService()
+    {
+        ContextDto contextDto = ContextFinder.GetById(Req.ContextId);
+        _createImpTablesRepository = new CreateImpTablesRepository(Mssql.GetInstanceByDto(contextDto));
+    }
+    
     public void Invoke()
     {
-        //db run 
+        _createImpTablesRepository.Invoke();
     }
 }
