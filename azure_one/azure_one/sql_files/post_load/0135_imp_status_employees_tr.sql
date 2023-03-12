545e7736be7740c171d4129c1cf79e6b1d716d70
ioedumac <eacevedof@yahoo.es>
2023-03-12 20:21:20 +0100

    fix tr

--- azure_one/azure_one/sql_files/post_load/0135_imp_status_employees_tr.sql
+++ azure_one/azure_one/sql_files/post_load/0135_imp_status_employees_tr.sql
@@ -8,8 +8,8 @@ INNER JOIN (
     vc_tr.mt_id as status_employees_id, 
     vli.lang_from as locale,
     CONVERT(VARCHAR(255),vc_tr.tr_i) as tr
-    FROM view_status_employees_tr [vc_tr]
-    INNER JOIN view_languages_index [vli]
+    FROM [local_staging].[dbo].view_status_employees_tr [vc_tr]
+    INNER JOIN [local_staging].[dbo].view_languages_index [vli]
     ON [vc_tr].[tr_num] = [vli].[tr_num]
 ) imp
 ON mt.status_employees_employee_status = imp.status_employees_id
@@ -27,8 +27,8 @@ FROM (
         vli.lang_from as locale, 
         CONVERT(VARCHAR(255),vc_tr.tr_i) as tr, 
         GETDATE() created_at 
-    FROM view_status_employees_tr [vc_tr]
-    INNER JOIN view_languages_index [vli]
+    FROM [local_staging].[dbo].view_status_employees_tr [vc_tr]
+    INNER JOIN [local_staging].[dbo].view_languages_index [vli]
     ON [vc_tr].[tr_num] = [vli].[tr_num]
 ) AS imp
 WHERE 1=1
