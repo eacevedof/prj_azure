38b1e2643971258f854ed6c196a5483a1bf70853
ioedumac <eacevedof@yahoo.es>
2023-03-12 11:13:41 +0100

    contexts

---
+++ azure_one/azure_one/Etl/InstallImpTables/Application/InstallImTablesService.cs
@@ -0,0 +1,10 @@
+namespace azure_one.Etl.InstallImpTables.Application;
+
+public sealed class InstallImTablesService
+{
+    
+    public void Ivoke()
+    {
+        
+    }
+}
--- azure_one/azure_one/Etl/Shared/Infrastructure/Db/Contexts/Context.cs
+++ azure_one/azure_one/Etl/Shared/Infrastructure/Db/Contexts/Context.cs
@@ -5,8 +5,21 @@ using azure_one.Etl.Shared.Infrastructure.Db;
 
 namespace azure_one.Etl.Shared.Infrastructure.Db.Contexts;
 
+/*
+ *
+ *  private const string _server = "localhost";
+        private const string _port = "1433";
+        private const string _database = "local_staging";
+        private const string _username = "sa";
+        private const string _password = "EafEaf1234";
+ */
 
 public sealed class Context
 {
-  
+    public string Server { get; set; }
+    public string Port { get; set; }
+    public string Database { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    
 }
