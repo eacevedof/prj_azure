d21a2ae2d07306d2a65aa02b749b2b4671abcb83
ioedumac <eacevedof@yahoo.es>
2023-03-13 23:18:37 +0100

    reset remove field

---
+++ azure_one/azure_one/sql_files/pre_load/0010_update_remove_fields.sql
@@ -0,0 +1,18 @@
+UPDATE [local_staging].[dbo].[imp_keys_and_values] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_errors] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_keys_and_values] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_languages] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_countries] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_provinces] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_cities] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_companies] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+-- UPDATE [local_staging].[dbo].[imp_languages_company_custom] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_user_types] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_status_employees] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_employees_departments] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_employees_positions] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_roles] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_employees] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_permissions] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_assets_types] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
+UPDATE [local_staging].[dbo].[imp_assets_types_attr] SET remove=NULL WHERE TRIM(COALESCE(remove,''))='';
