1277003f4f1961ca8cd2a5530efa973a74710c6e
ioedumac <eacevedof@yahoo.es>
2023-06-02 23:26:47 +0200

    backup

--- azure_one/azure_one/Etl/Shared/Infrastructure/Db/QueryBuilders/ReadQuery.cs
+++ azure_one/azure_one/Etl/Shared/Infrastructure/Db/QueryBuilders/ReadQuery.cs
@@ -1,42 +1,49 @@
-using System.IO;
-using azure_one.Etl.Shared.Infrastructure.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
 
 namespace azure_one.Etl.Shared.Infrastructure.Db.QueryBuilders;
 
 public sealed class ReadQuery
 {
-    private readonly string _pathFileSql;
+    
+    private string comment = "";
+    private string table = "";
+    private bool isDistinct = false;
+    private bool calcFoundRows = false;
+    private List<string> argFields = new List<string>();
+    private List<string> arJoins = new List<string>();
+    private List<string> arAnds = new List<string>();
+    private List<string> arGroupBy = new List<string>();
+    private List<string> arHaving = new List<string>();
+    private Dictionary<string, string> arOrderBy = new Dictionary<string, string>();
+    private Dictionary<string, int> arLimit = new Dictionary<string, int>();
+    private List<string> arEnd = new List<string>();
 
-    public ReadQuery(string pathFileSql)
-    {
-        _pathFileSql = pathFileSql;
-    }
+    private List<string> arNumeric = new List<string>();
+    private Dictionary<string, string> arInsertFv = new Dictionary<string, string>();
 
-    public static ReadQuery fromPrimitive(string pathFileSql)
+    private Dictionary<string, string> arUpdateFv = new Dictionary<string, string>();
+    private Dictionary<string, string> arPKs = new Dictionary<string, string>();
+    private List<string> select = new List<string>();
+
+    private string sql = "";
+    private string sqlCount = "";
+
+    private object oDB = null;
+
+    private List<string> reserved = new List<string> { "get", "order", "password" };
+
+    public const string READ = "r";
+    public const string WRITE = "w";
+
+    public static ReadQuery fromPrimitive()
     {
-        return (new(pathFileSql));
+        return (new());
     }
     
-    public void Invoke()
-    {
-        if (_pathFileSql.IsEmpty())
-        {
-            Lg.pr("no sql path file","ReadQuery");
-            return;
-        }
-        string sql = GetQueryContent() ?? "";
-        if (sql.IsEmpty())
-        {
-            Lg.pr("no sql data", "ReadQuery");
-            return;
-        }
-        Lg.pr(sql, _pathFileSql);
-        (new Mssql()).Execute(sql);
-    }
 
-    private string GetQueryContent()
-    {
-        return File.ReadAllText(_pathFileSql).Trim();
-    }
+    
 }
