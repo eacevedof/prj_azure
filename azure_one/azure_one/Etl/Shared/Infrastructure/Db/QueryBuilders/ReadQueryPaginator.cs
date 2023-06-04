eec7038df2609dc60fa963bc3c898a65942adb82
ioedumac <eacevedof@yahoo.es>
2023-06-04 20:01:40 +0200

    backup

--- azure_one/azure_one/Etl/Shared/Infrastructure/Db/QueryBuilders/ReadQueryPaginator.cs
+++ azure_one/azure_one/Etl/Shared/Infrastructure/Db/QueryBuilders/ReadQueryPaginator.cs
@@ -9,6 +9,7 @@ namespace azure_one.Etl.Shared.Infrastructure.Db.QueryBuilders;
 public sealed class ReadQueryPaginator
 {
     
+    private Mssql _mssql = null;
     private ReadQuery _readQuery = null;
     private int _page = 1;
     private int _pageSize = 1;
@@ -16,6 +17,7 @@ public sealed class ReadQueryPaginator
     private int _totalRows = 0;
     private int _offsetStart = 0;
     private int _totalPages = 0;
+
 
     public ReadQueryPaginator(ReadQuery readQuery, int page, int pageSize)
     {
@@ -27,11 +29,19 @@ public sealed class ReadQueryPaginator
         _pageSize = pageSize;
         if (_pageSize < 1) 
             throw new Exception("wrong page size");
+
+        _mssql = Mssql.GetInstanceByReq();
     }
     
     public static ReadQueryPaginator fromPrimitives(ReadQuery readQuery, int page, int pageSize)
     {
         return new ReadQueryPaginator(readQuery, page, pageSize);
+    }
+
+    public ReadQueryPaginator Calculate()
+    {
+        
+        return this;
     }
 
 
