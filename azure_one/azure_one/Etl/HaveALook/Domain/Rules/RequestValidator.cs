4287408f5f345bb877ee49e64f8a9f711bb88ca9
ioedumac <eacevedof@yahoo.es>
2023-06-07 22:30:47 +0200

    paginator

---
+++ azure_one/azure_one/Etl/HaveALook/Domain/Rules/RequestValidator.cs
@@ -0,0 +1,13 @@
+
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace azure_one.Etl.HaveALook.Domain.Rules;
+
+public sealed class RequestValidator
+{   
+    public void Invoke(HttpRequest httpRequest)
+    {
+      
+    }
+}
