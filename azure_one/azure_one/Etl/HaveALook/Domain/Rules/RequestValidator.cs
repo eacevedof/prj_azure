41005238853486badc7beef8ea9a17de8d2b9ce2
ioedumac <eacevedof@yahoo.es>
2023-06-07 22:33:43 +0200

    paginator

--- azure_one/azure_one/Etl/HaveALook/Domain/Rules/RequestValidator.cs
+++ azure_one/azure_one/Etl/HaveALook/Domain/Rules/RequestValidator.cs
@@ -8,6 +8,15 @@ public sealed class RequestValidator
 {   
     public void Invoke(HttpRequest httpRequest)
     {
-      
+        IQueryCollection query = httpRequest.Query;
+        query.TryGetValue
+
+        /*
+      int page = int.Parse(req.Query["page"]);
+                int perPage = int.Parse(req.Query["per_page"]);
+                string search = req.Query["search"];
+                string orderBy = req.Query["order_by"];
+                string orderColumn = req.Query["order_column"];        
+        */
     }
 }
