ca4d2deb1c0e17285c227cebebdade5474c0e8be
ioedumac <eacevedof@yahoo.es>
2023-06-07 22:40:34 +0200

    paginator

--- azure_one/azure_one/Etl/HaveALook/Domain/Rules/RequestValidator.cs
+++ azure_one/azure_one/Etl/HaveALook/Domain/Rules/RequestValidator.cs
@@ -9,8 +9,21 @@ public sealed class RequestValidator
     public void Invoke(HttpRequest httpRequest)
     {
         IQueryCollection query = httpRequest.Query;
-        query.TryGetValue
+        if (!query.ContainsKey("page"))
+            throw new Exception("missing page");
 
+        string value = query["page"];
+        if (!IsInteger(value))
+            throw new Exception("page must be integer");
+        
+        if (!query.ContainsKey("per_page"))
+            throw new Exception("missing per_page");
+
+        value = query["per_page"];
+        if (!IsInteger(value))
+            throw new Exception("per_page must be integer");
+        
+        
         /*
       int page = int.Parse(req.Query["page"]);
                 int perPage = int.Parse(req.Query["per_page"]);
@@ -18,5 +31,10 @@ public sealed class RequestValidator
                 string orderBy = req.Query["order_by"];
                 string orderColumn = req.Query["order_column"];        
         */
+    }
+
+    private bool IsInteger(string value)
+    {
+        return int.TryParse(value, out int result);
     }
 }
