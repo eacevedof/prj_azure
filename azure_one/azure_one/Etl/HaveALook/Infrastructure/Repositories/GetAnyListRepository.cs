1865811872a0c4805656936b5e4d09e605ce9a57
ioedumac <eacevedof@yahoo.es>
2023-06-07 21:32:20 +0200

    paginator

--- azure_one/azure_one/Etl/HaveALook/Infrastructure/Repositories/GetAnyListRepository.cs
+++ azure_one/azure_one/Etl/HaveALook/Infrastructure/Repositories/GetAnyListRepository.cs
@@ -51,49 +51,4 @@ public sealed class GetAnyListRepository: AbsRepository
         );
         
     }
-
-    public void Invoke_(string table)
-    {
-        Lg.pr("GetAnyListRepository started!");
-        string sqlPaginate = $"SELECT * FROM {table}";
-
-        ReadQuery readQuery = ReadQuery.fromTable("[imp_provinces] p");
-
-        readQuery.SetComment("primera query");
-
-        readQuery
-            .AddGetField("p.tenant_slug")
-            .AddGetField("p.countries_uuid")
-            .AddGetField("p.countries_id")
-            .AddGetField("MAX(p.val) province")
-            //.AddGetField("p.codesap AS p_codesap")
-            //.AddGetField("c.codesap AS c_codesap")
-            .AddGetField("MAX(c.val) AS country")
-            .AddJoin(@"INNER JOIN [imp_countries] c ON p.countries_id = c.countries_id")
-            .AddWhere("p.id > 3").AddWhere("c.id > 5")
-            .AddGroupBy("p.countries_uuid")
-            .AddGroupBy("p.tenant_slug")
-            .AddGroupBy("p.countries_id")
-            .AddHaving("LEN(p.countries_id) > 2")
-            .AddOrderBy("p.countries_uuid", ReadQuery.ORDER_DESC)
-            .setOffset(8, 13)
-        ;
-
-        string sql = readQuery.Select().GetSql();
-        string sqlCount = readQuery.GetSqlCount();
-
-        List<Dictionary<string, string>> result = _db.Query(sql);
-        List<Dictionary<string, string>> count = _db.Query(sqlCount);
-
-        foreach (var dict in result) 
-        {
-            string id = dict["id"];
-            Lg.pr($"id: {id}");
-        }
-        Lg.pr("GetAnyListRepository finished!");
-    }
-
-
-
-
 }
