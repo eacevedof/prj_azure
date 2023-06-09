de8e04645ddb399ca2cdd879626d129ba4f9ea67
ioedumac <eacevedof@yahoo.es>
2023-06-09 23:01:28 +0200

    abstrac have look

--- azure_one/azure_one/Etl/HaveALook/Domain/Exceptions/HaveALookException.cs
+++ azure_one/azure_one/Etl/HaveALook/Domain/Exceptions/HaveALookException.cs
@@ -11,22 +11,22 @@ public class HaveALookException : AbstractHaveALookException
 
     public static void FailIfMissingPage()
     {
-        throw new HaveALookException("missing page");
+        throw new HaveALookException("missing page", AbstractHaveALookException.BAD_REQUEST_CODE);
     }
 
     public static void FailIfPageIsNotInteger()
     {
-        throw new HaveALookException("page must be a integer");
+        throw new HaveALookException("page must be a integer", AbstractHaveALookException.BAD_REQUEST_CODE);
     }
 
     public static void FailIfMissingPerPage()
     {
-        throw new HaveALookException("per_page is not provided");
+        throw new HaveALookException("per_page is not provided", AbstractHaveALookException.BAD_REQUEST_CODE);
     }
 
     public static void FailIfPerPageIsNotInteger()
     {
-        throw new HaveALookException("per_page must be a integer");
+        throw new HaveALookException("per_page must be a integer", AbstractHaveALookException.BAD_REQUEST_CODE);
     }
 
 
