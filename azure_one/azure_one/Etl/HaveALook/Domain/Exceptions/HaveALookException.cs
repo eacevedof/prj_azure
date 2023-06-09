7f69606cb22e6ddeca359510c1179d919c211f52
ioedumac <eacevedof@yahoo.es>
2023-06-09 23:16:14 +0200

    read-query

--- azure_one/azure_one/Etl/HaveALook/Domain/Exceptions/HaveALookException.cs
+++ azure_one/azure_one/Etl/HaveALook/Domain/Exceptions/HaveALookException.cs
@@ -29,5 +29,14 @@ public class HaveALookException : AbstractHaveALookException
         throw new HaveALookException("per_page must be a integer", AbstractHaveALookException.BAD_REQUEST_CODE);
     }
 
+    public static void FailIfPageIsLowerThanOne()
+    {
+        throw new HaveALookException("page can not be lower than 1", AbstractHaveALookException.BAD_REQUEST_CODE);
+    }
+
+    public static void FailIfPerPageIsLowerThanOne()
+    {
+        throw new HaveALookException("per_page can not be lower than 1", AbstractHaveALookException.BAD_REQUEST_CODE);
+    }
 
 }
