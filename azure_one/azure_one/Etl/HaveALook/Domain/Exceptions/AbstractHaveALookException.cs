a9b8441cfc3c153262a9aa1326358de35c47e81c
ioedumac <eacevedof@yahoo.es>
2023-06-09 23:12:43 +0200

    read-query

--- azure_one/azure_one/Etl/HaveALook/Domain/Exceptions/AbstractHaveALookException.cs
+++ azure_one/azure_one/Etl/HaveALook/Domain/Exceptions/AbstractHaveALookException.cs
@@ -5,7 +5,7 @@ namespace azure_one.Etl.HaveALook.Domain.Exceptions;
 public abstract class AbstractHaveALookException : Exception
 {
     protected int _code = 0;
-    protected const int BAD_REQUEST = 400;
+    protected const int BAD_REQUEST_CODE = 400;
 
     public AbstractHaveALookException(string message, int code = 0) : base(message)
     {
