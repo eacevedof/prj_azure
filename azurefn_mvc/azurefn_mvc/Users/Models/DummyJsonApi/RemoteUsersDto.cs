fb25594f47e90ecc9df0ed9f478866934e174807
ioedumac <eacevedof@yahoo.es>
2023-10-22 18:57:26 +0200

    list ok

--- azurefn_mvc/azurefn_mvc/Users/Models/DummyJsonApi/RemoteUsersDto.cs
+++ azurefn_mvc/azurefn_mvc/Users/Models/DummyJsonApi/RemoteUsersDto.cs
@@ -4,7 +4,6 @@ namespace Fn.Users.Models.DummyJsonApi
 {
     public sealed class RemoteUsersDto
     {
-        //public List<object> users { get; set; }
         public object users { get; set; }
         public int total { get; set; }
         public int skip { get; set; }
