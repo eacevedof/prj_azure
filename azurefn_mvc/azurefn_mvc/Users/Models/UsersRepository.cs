e8a9d66b3eeb268e545ae0863de3ccf0d5a801b7
ioedumac <eacevedof@yahoo.es>
2023-10-22 16:19:23 +0200

    get data

--- azurefn_mvc/azurefn_mvc/Users/Models/UsersRepository.cs
+++ azurefn_mvc/azurefn_mvc/Users/Models/UsersRepository.cs
@@ -1,14 +1,32 @@
+using System;
+using System.Net;
 using System.Collections.Generic;
+using System.Text.Json;
+
 
 namespace Fn.Users.Models
 {
-
     public sealed class UsersRepository
     {
+        private string USERS_ENDPOINT = "https://dummyjson.com/users";
+
         public List<Dictionary<string,string>> GetUsersBySearchText(string searchText)
         {
-            List<Dictionary<string, string>> list = new();
+            var json = _GetUsersFromEndpoint();
+            List<Dictionary<string, string>> list = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(json);
             return list;
         }
+
+        private string _GetUsersFromEndpoint()
+        {
+            string content = "";
+            using (WebClient client = new WebClient())
+            {
+                content = client.DownloadString(USERS_ENDPOINT);
+                Console.WriteLine(content);
+            }
+            return content;
+        }
+
     }
 }
