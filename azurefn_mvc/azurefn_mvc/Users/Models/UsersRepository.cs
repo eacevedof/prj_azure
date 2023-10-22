7fc79c31f709bd2b9de99be3cb0abc3f2cf62ecf
ioedumac <eacevedof@yahoo.es>
2023-10-22 18:16:09 +0200

    remote-users-dto

--- azurefn_mvc/azurefn_mvc/Users/Models/UsersRepository.cs
+++ azurefn_mvc/azurefn_mvc/Users/Models/UsersRepository.cs
@@ -13,25 +13,19 @@ namespace Fn.Users.Models
         public List<Dictionary<string,string>> GetUsersBySearchText(string searchText)
         {
             var json = _GetUsersJsonFromEndpoint();
-            //List<Object> list = JsonSerializer.Deserialize<List<Object>>(json);
-
             RemoteUsersDto remoteUsersDto = JsonSerializer.Deserialize<RemoteUsersDto>(json);
-            //List<RemoteUserDto> remoteUsers = JsonSerializer.Deserialize<List<RemoteUserDto>>(remoteUsersDto.users.ToString());
-
-            var usersFound = new List<Dictionary<string, string>>();
             List<RemoteUserDto> remoteUsers = JsonSerializer.Deserialize<List<RemoteUserDto>>(remoteUsersDto.users.ToString());
-            /*
-            foreach (var o in remoteUsersDto.users)
+
+            var usersFound = new List<Dictionary<string, string>>();            
+            foreach (var remoteUserDto in remoteUsers)
             {
-                var x = o.ToString();
-                List<RemoteUserDto> remoteUsers = JsonSerializer.Deserialize<List<RemoteUserDto>>(o.ToString());
                 var dic = new Dictionary<string, string>();
-                dic["id"] = _GetAttributeValue(o, "id");
-                dic["name"] = _GetAttributeValue(o, "firstName") + " " + _GetAttributeValue(o, "lastName");
-                dic["email"] = _GetAttributeValue(o, "email");
+                dic["id"] = remoteUserDto.id.ToString();
+                dic["name"] = $"{remoteUserDto.firstName} {remoteUserDto.lastName}";
+                dic["email"] = remoteUserDto.email;
                 usersFound.Add(dic);
             }
-            */
+            
             return usersFound;
         }
 
